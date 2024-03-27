536ab680cfd919ddb6d1e1d40df74c24e5f9ce60
Ramanathan E.M <137532243+ramanathanem@users.noreply.github.com>
2024-03-27 12:30:49 +0530

    Update RecentJobPost.jsx

--- src/Components/RecentJobPost.jsx
+++ src/Components/RecentJobPost.jsx
@@ -9,7 +9,7 @@ const RecentJobPost = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/job/getdata');
+      const response = await axios.get('https://portal-backend-w2dr.onrender.com/job/getdata');
       setData(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);

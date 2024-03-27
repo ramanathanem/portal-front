494104cea600c620da0b23c9d4b0050a407bdb04
Ramanathan E.M <137532243+ramanathanem@users.noreply.github.com>
2024-03-27 12:16:32 +0530

    Update Humanresource.jsx

--- src/Components/Humanresource.jsx
+++ src/Components/Humanresource.jsx
@@ -10,7 +10,7 @@ function Humanresource() {
 
     const fetchData = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/job/getdata'); 
+            const response = await axios.get('https://portal-backend-w2dr.onrender.com/job/getdata'); 
             const filteredData = response.data.filter(entry => entry.category === "HumanResource");
             setData(filteredData);
         } catch (error) {
